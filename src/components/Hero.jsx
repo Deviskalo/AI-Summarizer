611a152d13d29f5b13d1d41cbe9bb5d9bacfc564
Dev Iskalo <99690368+Deviskalo@users.noreply.github.com>
2023-06-02 19:30:54 +0000

    Adding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt="sumz_logo"  className='w-28 object-contain'/>
-        <button type='button' onClick={() => window.open('https://github.com/deviskalo/')} className='black_btn'>
+        <button type='button' onClick={() => window.open('https://github.com/Deviskalo/AI-Summarizer')} className='black_btn'>
           Github
         </button>
       </nav>
